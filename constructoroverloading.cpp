31e0f498e0e147daaf0ae9e2b609db569167bd98
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-05-13 19:24:06 +0700

    definisi NIM dan NAMA

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -24,3 +24,8 @@ mahasiswa :: mahasiswa(int iNim){
 mahasiswa :: mahasiswa(string iNama){
     nama = iNama; //definisi hanya Nama 
 }
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; //definisi NIM dan NAMA
+    nama = iNama;
+}
